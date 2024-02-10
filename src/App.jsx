a5dbc2604075925d5f954e92807a42bd096f8f8c
Kalpesh Dhoke <38274099+kalpesh-d@users.noreply.github.com>
2024-02-10 18:37:06 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { gql, useQuery } from "@apollo/client";
 
-const ALL_FILMS = gql`
-  query Query($after: String, $before: String, $first: Int, $last: Int) {
-    allFilms(after: $after, before: $before, first: $first, last: $last) {
-      films {
-        director
-        id
-        releaseDate
-        title
-      }
-    }
-  }
-`;
+// const ALL_FILMS = gql`
+//   query Query($after: String, $before: String, $first: Int, $last: Int) {
+//     allFilms(after: $after, before: $before, first: $first, last: $last) {
+//       films {
+//         director
+//         id
+//         releaseDate
+//         title
+//       }
+//     }
+//   }
+// `;
 
 function App() {
-  const Films = useQuery(ALL_FILMS);
-  console.log(Films);
-  if (Films.loading) return <div>Loading...</div>;
+  // const Films = useQuery(ALL_FILMS);
+  // console.log(Films);
+  // if (Films.loading) return <div>Loading...</div>;
+  // return <div>{Films.data.allFilms.films.map((p) => p.title)}</div>;
 
-  return <div>{Films.data.allFilms.films.map((p) => p.title)}</div>;
+  return (
+    <div className="grid grid-rows-3">
+      <headers className="bg-indigo-500">1</headers>
+      <mainain className="bg-amber-500">1</mainain>
+    </div>
+  );
 }
 
 export default App;
