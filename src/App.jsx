8eadfa584eb8a3b22b0e2183f719442575f8b4ca
Kalpesh Dhoke <38274099+kalpesh-d@users.noreply.github.com>
2024-02-10 17:48:16 +0530

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-function App() {}
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_FILMS = gql`
+  query Query($after: String, $before: String, $first: Int, $last: Int) {
+    allFilms(after: $after, before: $before, first: $first, last: $last) {
+      films {
+        director
+        id
+        releaseDate
+        title
+      }
+    }
+  }
+`;
+
+function App() {
+  const Films = useQuery(ALL_FILMS);
+  console.log(Films);
+  if (Films.loading) return <div>Loading...</div>;
+
+  return <div>{Films.data.allFilms.films.map((p) => p.title)}</div>;
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query Query($after: String, $before: String, $first: Int, $last: Int) {
+    allFilms(after: $after, before: $before, first: $first, last: $last) {
+      edges {
+        cursor
+        node {
+          id
+          director
+          title
+          releaseDate
+        }
+      }
+    }
+  }
+`;
+
+client.query({ query });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
